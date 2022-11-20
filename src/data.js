57f520e44b11812c01f4303e63622622447c3376
abdulmalik03 <md.malik.0311@gmail.com>
2022-11-20 11:56:24 +0530

    Slider and Categories

---
+++ src/data.js
@@ -0,0 +1,92 @@
+export const sliderItems = [
+  {
+    id: 1,
+    img: "https://myfoodstory.com/wp-content/uploads/2018/09/The-Best-Chicken-Biryani-Recipe-1.jpg",
+    title: "WHAT ARE WE?",
+    desc: "FORGET EXISTING PARTIES. FIND FUN ON COOKING PARTIES",
+    bg: "f5fafd",
+    button: "SHOP NOW",
+  },
+  {
+    id: 2,
+    img: "https://cdn.shopify.com/s/files/1/0607/0986/3661/products/chicken-web-sirte-images.jpg?v=1663578704",
+    title: "MEAT COLLECTION",
+    desc: "ONE STOP PLACE FOR NV LOVERS. AND WE NOT ONLY STOP THERE",
+    bg: "fcf1ed",
+    button: "SHOP NOW",
+  },
+  {
+    id: 3,
+    img: "https://www.simplyrecipes.com/thmb/rngm-7eZfo-gsKuRWOceBMM9m_c=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/Simply-Recipes-Homemade-Pizza-Dough-Lead-Shot-1b-ea13798d224048b3a28afb0936c9b645.jpg",
+    title: "HURRAY!! WE ALSO PROVIDE RECIPES",
+    desc: "WE KNOW THERE IS NO FUN WITHOUT UNKNOWN DISHES. KICK START WITH OUR RECIPES",
+    bg: "fbf0f4",
+    button: "GIVE A READ",
+  },
+  {
+    id: 4,
+    img: "https://cdn.cdnparenting.com/articles/2019/03/04141719/1276599535-H-1024x700.jpg",
+    title: "WE CARE FOR YOUR COOKING. INTRODUCING INDIAN SPICES",
+    desc: "",
+    bg: "fbf0f4",
+    button: "SHOP NOW",
+  },
+];
+
+export const categories = [
+  {
+    id: 1,
+    img: "https://cdn.shopify.com/s/files/1/0607/0986/3661/products/chicken-web-sirte-images.jpg?v=1663578704",
+    title: "CHICKEN CUTS!",
+  },
+  {
+    id: 2,
+    img: "https://www.everydaymeat.com/wp-content/uploads/2019/11/mutton_boneless.jpg",
+    title: "LAMB LOVERS",
+  },
+  {
+    id: 3,
+    img: "https://www.spicejungle.com/wp/spice/files/2015/10/how-to-properly-store-spices.jpg",
+    title: "SPICE IT UP",
+  },
+  {
+    id: 4,
+    img: "https://ik.imagekit.io/bfrs/tr:w-1001,h-1001,pr-true,cm-pad_resize,bg-FFFFFF/image_crafterscornerdemo/data/DMCMA487.jpg",
+    title: "APPLIANCES",
+  },
+];
+
+export const popularProducts = [
+  {
+    id: 1,
+    img: "https://d3o2e4jr3mxnm3.cloudfront.net/Mens-Jake-Guitar-Vintage-Crusher-Tee_68382_1_lg.png",
+  },
+  {
+    id: 2,
+    img: "https://cdn.shopify.com/s/files/1/0101/4832/products/Angela_Natural_Tee.png?v=1606780388",
+  },
+  {
+    id: 3,
+    img: "https://www.prada.com/content/dam/pradanux_products/U/UCS/UCS319/1YOTF010O/UCS319_1YOT_F010O_S_182_SLF.png",
+  },
+  {
+    id: 4,
+    img: "https://www.burdastyle.com/pub/media/catalog/product/cache/7bd3727382ce0a860b68816435d76e26/107/BUS-PAT-BURTE-1320516/1170x1470_BS_2016_05_132_front.png",
+  },
+  {
+    id: 5,
+    img: "https://images.ctfassets.net/5gvckmvm9289/3BlDoZxSSjqAvv1jBJP7TH/65f9a95484117730ace42abf64e89572/Noissue-x-Creatsy-Tote-Bag-Mockup-Bundle-_4_-2.png",
+  },
+  {
+    id: 6,
+    img: "https://d3o2e4jr3mxnm3.cloudfront.net/Rocket-Vintage-Chill-Cap_66374_1_lg.png",
+  },
+  {
+    id: 7,
+    img: "https://www.vintageindustries.nl/download_front/qympzk1762/2217_Arrow_Jacket_Forest.png",
+  },
+  {
+    id: 8,
+    img: "https://www.pngarts.com/files/3/Women-Jacket-PNG-High-Quality-Image.png",
+  },
+];
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,16 @@
 import React from 'react'
 import Navbar from '../components/Navbar'
 import Announce from '../components/Announce'
+import Slider from '../components/Slider'
+import Categories from '../components/Categories'
 
 const Home = () => {
   return (
     <div>
       <Announce/>
-      <Navbar/>  
+      <Navbar/> 
+      <Slider/>
+      <Categories/> 
     </div>  
   )
 }
